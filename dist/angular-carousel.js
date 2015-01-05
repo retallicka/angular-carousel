4db8f4515cbc219d1965989a09ec1f4aa7e8e770
Victoria Retallick <vretallick@zedge.net>
2015-01-05 10:07:52 +0100

    Redo changes on refactored carousel
    These changes needed to be reapplied with refactor:
    - emit 'carouselScrolled' event
    - shorten scrolling duration
    - remove orientation binding

--- dist/angular-carousel.js
+++ dist/angular-carousel.js
@@ -352,6 +352,9 @@ angular.module('angular-carousel').run(['$templateCache', function($templateCach
                             angular.forEach(getSlidesDOM(), function(child, index) {
                                 child.style.cssText = createStyleString(computeCarouselSlideStyle(index, x, options.transitionType));
                             });
+
+                            var c = (x/100) * getContainerWidth();
+                            scope.$emit('carouselScrolled', getSlidesDOMParent(), c);
                         }
 
                         scope.nextSlide = function(slideOptions) {
@@ -628,22 +631,6 @@ angular.module('angular-carousel').run(['$templateCache', function($templateCach
                                 }, 0, false);
                             }
                         }
-
-                        function onOrientationChange() {
-                            updateContainerWidth();
-                            goToSlide();
-                        }
-
-                        // handle orientation change
-                        var winEl = angular.element($window);
-                        winEl.bind('orientationchange', onOrientationChange);
-                        winEl.bind('resize', onOrientationChange);
-
-                        scope.$on('$destroy', function() {
-                            $document.unbind('mouseup', documentMouseUpEvent);
-                            winEl.unbind('orientationchange', onOrientationChange);
-                            winEl.unbind('resize', onOrientationChange);
-                        });
                     };
                 }
             };
